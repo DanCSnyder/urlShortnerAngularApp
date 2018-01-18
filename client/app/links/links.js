9cc54eca191e77e9a018965f70b82f4226cd5956
Daniel Snyder <daniel.snyder@icharts.net>
2018-01-18 11:07:22 -0800

    Add links functionality.
    Direct people to correct urls that get shorten

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -2,5 +2,17 @@ angular
   .module("shortly.links", [])
 
   .controller("LinksController", function($scope, Links) {
-    //
+    $scope.data = {};
+
+    var initializeLinks = function() {
+      Links.getAll()
+        .then(function(links) {
+          $scope.data.links = links;
+        })
+        .catch(function(error) {
+          console.error(error);
+        });
+    };
+
+    initializeLinks();
   });
